9d4b9c656f72998d1de069fc90d6a911adc92697
sansyo <sansyo78@gmail.com>
2022-04-12 21:25:53 +0900

    first commit

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controller;
+
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("test") //리소스
+public class TestController {
+	@GetMapping
+	public String testController() {
+		return "Hello World!!";
+	}
+	
+	@GetMapping("/testGetMapping")
+	public String testControllerWithPath() {
+		return "Hello World!! testGetMapping";
+	}
+	
+	@GetMapping("/{id}")
+	public String testControllerWithPathVariables(@PathVariable(required=false) int id) {
+		return "Hello World!!! ID " + id;
+	}
+	
+	@GetMapping("/testRequestParam")
+	public String testControllerRequestParam(@RequestParam(required=false) int id) {
+		return "Hello World! RequestParam ID " + id;
+	}
+	
+	@GetMapping("/testRequestBody")
+	public ResponseDTO<String> testControllerRequestBody(@RequestBody TestRequestBodyDTO dto) {
+		List<String> list = new ArrayList<>();
+		list.add("Hello World! i am ResponseDTO");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		
+		return response;
+	}
+	
+	@GetMapping("/testResponseEntity")
+	public ResponseEntity<?> testControllerResponseEntity() {
+		List<String> list = new ArrayList<>();
+		list.add("Hello World! i am ResponseEntity And you got 400");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		
+		return ResponseEntity.badRequest().body(response);
+	}
+	
+	
+	
+
+}
